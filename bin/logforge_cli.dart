82717d729ed37961301d4cc2d573c925796c6f05
r4khul <blackempire9177@gmail.com>
2025-12-03 23:14:30 +0530

    init: intial project setup for dart

---
+++ bin/logforge_cli.dart
@@ -0,0 +1,5 @@
+import 'package:logforge_cli/logforge_cli.dart' as logforge_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${logforge_cli.calculate()}!');
+}
---
+++ test/logforge_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:logforge_cli/logforge_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
